379848d8700ba71ebdea275715ca39c6d3e8b9bd
vladimirrim <vladimirrim98@gmail.com>
2018-09-25 23:06:31 +0300

    Style fixes.

--- hw_3/main.py
+++ hw_3/main.py
@@ -2,31 +2,26 @@ import numpy.random as ra
 import math
 import matplotlib.pyplot as plt
 
-k = [i for i in range(100)]
-
-uniValues = []
-expValues = []
-
 LAMBDA = 1
 THETA = 1
 
 
-def calculateUni(k):
-    meanUni = 0
+def calculate_uni(k):
+    mean_uni = 0
     values = ra.uniform(0, THETA, 100)
     for val in values:
-        meanUni += val ** k
-    meanUni /= 100
-    return ((k + 1) * meanUni) ** (1 / k)
+        mean_uni += val ** k
+    mean_uni /= 100
+    return ((k + 1) * mean_uni) ** (1 / k)
 
 
-def calculateExp(k):
-    meanExp = 0
+def calculate_exp(k):
+    mean_exp = 0
     values = ra.exponential(LAMBDA, 100)
     for val in values:
-        meanExp += val ** k
-    meanExp /= 100
-    return (math.factorial(k) * meanExp) ** (1 / k)
+        mean_exp += val ** k
+    mean_exp /= 100
+    return (math.factorial(k) * mean_exp) ** (1 / k)
 
 
 if __name__ == '__main__':
@@ -35,11 +30,11 @@ if __name__ == '__main__':
     uniValues = []
     expValues = []
     for i in range(1, 50):
-        deviation = sum([(LAMBDA - calculateExp(i)) ** 2 for _ in range(50)])
+        deviation = sum([(LAMBDA - calculate_exp(i)) ** 2 for _ in range(50)])
         expValues.append((deviation / 50) ** 0.5)
 
     for i in range(1, 50):
-        deviation = sum([(THETA - calculateUni(i)) ** 2 for _ in range(50)])
+        deviation = sum([(THETA - calculate_uni(i)) ** 2 for _ in range(50)])
         uniValues.append((deviation / 50) ** 0.5)
 
     plt.plot(k, uniValues)
